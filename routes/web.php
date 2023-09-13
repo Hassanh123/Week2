9561fbf7ae58eca58fbac3650d7982703b53d75f
Hassan <ps197137@summacollege.nl>
2023-09-13 11:41:52 +0200

    Eerste commit

--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+use App\Http\Controllers\StudentController;
+
+Route::get('/student/{naamUitUrl}',[StudentController::class, 'show']);
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
