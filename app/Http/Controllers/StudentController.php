7da6f1090ec0e487ea01365b1a3f92f049cb7c62
Hassan <ps197137@summacollege.nl>
2023-09-13 11:53:36 +0200

    Nieuwste commit

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Str;
 class StudentController extends Controller
 {
-    
+    public function show($naamUitUrl)
+    {
+        $naamUitUrl = Str::upper($naamUitUrl);
+        return view('student', ['naam' => $naamUitUrl]);
+    }
+
+
 
 }
 
